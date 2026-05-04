c3567e34de5cec40411a3f101beb473f0053a323
FedericoCAlonso <federico.c.alonso@outlook.com.ar>
2026-05-04 14:00:58 -0300

    forzar deploy 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@
 // ═══════════════════════════════════════════════════════════════════════════
 
 
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { useProjects } from './hooks/useProjects';
